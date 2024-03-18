fec8353d2cdd7d7d73257efa978427fa7fbb776c
bhaveshj2611 <bhaveshjoshi2611@gmail.com>
2024-03-18 22:33:46 +0530

    minor

--- lib/widgets/appBar.dart
+++ lib/widgets/appBar.dart
@@ -5,18 +5,18 @@ buildAppBar({List<Widget>? actions, required String text, Widget? leading}) {
   return AppBar(
     elevation: 3,
     shadowColor: AppColor.fifth.withOpacity(0.2),
-    backgroundColor: AppColor.first,
+    backgroundColor: AppColor.second,
     shape: const RoundedRectangleBorder(
       borderRadius: BorderRadius.vertical(
         bottom: Radius.circular(20.0),
       ),
     ),
-    iconTheme: const IconThemeData(color: AppColor.sixth),
+    iconTheme: const IconThemeData(color: AppColor.fifth),
     toolbarHeight: 80,
     title: Text(
       text,
       style: const TextStyle(
-          fontSize: 20, color: AppColor.sixth, fontWeight: FontWeight.bold),
+          fontSize: 20, color: AppColor.fifth, fontWeight: FontWeight.bold),
     ),
     actions: actions,
     leading: leading,
